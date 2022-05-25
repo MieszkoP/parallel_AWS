8726073d3a11129252e9206cee3ec7404cff1c26
MieszkoP <78937784+MieszkoP@users.noreply.github.com>
2022-05-26 00:20:28 +0200

    Add files via upload

--- data.sh
+++ data.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+mkdir text
+cd text
 wget https://www.i3s.unice.fr/~jplozi/hadooplab_lsds_2015/datasets/gutenberg-500M.txt.gz
+gzip -d gutenberg-500M.txt.gz
+for i in {1..10}
+do
+    cp gutenberg-500M.txt gutenberg-$i.txt
+done
+        
+
+##hdfs dfs -mkdir text-input
+##hdfs dfs -put *.txt books-input
