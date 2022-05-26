f665a149672822925b9dece2177c4474e8c90c4b
MieszkoP <78937784+MieszkoP@users.noreply.github.com>
2022-05-26 02:05:02 +0200

    Update data.sh

--- data.sh
+++ data.sh
@@ -4,7 +4,7 @@ mkdir text
 cd text
 wget https://www.i3s.unice.fr/~jplozi/hadooplab_lsds_2015/datasets/gutenberg-500M.txt.gz
 gzip -d gutenberg-500M.txt.gz
-for i in {1..10}
+for i in {1..2}
 do
     cp gutenberg-500M.txt gutenberg-$i.txt
 done
@@ -12,3 +12,5 @@ rm gutenberg-500M.txt
 
 hdfs dfs -mkdir text-input
 hdfs dfs -put *.txt text-input
+
+time hadoop jar /usr/lib/hadoop/hadoop-streaming.jar -files mapper.py,reducer.py -mapper mapper.py -reducer reducer.py -input text-input -output text-output
